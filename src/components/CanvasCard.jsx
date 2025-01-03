dcce4fc4b4aa4a817003ec8ce1c3b5a20eacba58
barleymilk <barleymilk640@gmail.com>
2025-01-03 12:19:46 +0900

    노트 추가 버튼

--- src/components/CanvasCard.jsx
+++ src/components/CanvasCard.jsx
@@ -1,28 +1,32 @@
+import { v4 as uuidv4 } from 'uuid';
 import { FaPlus } from 'react-icons/fa';
 import Note from './Note';
-
 function CanvasCard({ title, isSubtitle = false, notes = [], onNotesChange }) {
-  // const [notes, setNotes] = useState([]);
   const handleAddNote = () => {
-    // setNotes([...notes, { id: uuidv4(), content: '' }]);
+    const newNote = {
+      id: uuidv4(),
+      content: '',
+      color: '',
+    };
+    onNotesChange([...notes, newNote]);
   };
   const handleRemoveNote = id => {
-    // setNotes(notes.filter(note => note.id !== id));
+    onNotesChange(notes.filter(note => note.id !== id));
   };
-  const handleUpdateNote = (id, content, color) =>
+  const handleUpdateNote = (id, content, color) => {
     onNotesChange(
       notes.map(note => (note.id === id ? { ...note, content, color } : note)),
     );
-
+  };
   return (
     <div className="row-span-1 bg-white min-h-48 border border-collapse border-gray-300">
       <div
         className={`${isSubtitle === false && 'bg-gray-100 border-b border-b-gray-300'} flex items-start justify-between px-3 py-2`}
       >
-        <h3 className={`${isSubtitle === false && 'font-bold'}`}>{title}</h3>
+        <h3 className={`${isSubtitle === false && 'font-bold'} `}>{title}</h3>
         <button
-          onClick={handleAddNote}
           className="bg-blue-400  text-white p-1.5 text-xs rounded-md"
+          onClick={handleAddNote}
         >
           <FaPlus />
         </button>
@@ -33,9 +37,9 @@ function CanvasCard({ title, isSubtitle = false, notes = [], onNotesChange }) {
             key={note.id}
             id={note.id}
             content={note.content}
+            color={note.color}
             onRemoveNote={handleRemoveNote}
             onUpdateNote={handleUpdateNote}
-            color={note.color}
           />
         ))}
       </div>
